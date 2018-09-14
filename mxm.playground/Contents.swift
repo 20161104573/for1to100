bef9184b406493e658ed53659ebd41e43b871061
20161104573 <a20161104573@mac05.local>
2018-09-14 17:27:16 +0800

    新增两个模块
    增加两个数相加和十个数排序

--- mxm.playground/Contents.swift
+++ mxm.playground/Contents.swift
@@ -3,9 +3,29 @@
 import UIKit
 
 //import PlaygroundSupport
-
+//一到一百相加
 var x=0
 for i in 1...100{
     x=x+i
 }
-  print(x)
+//两个数相加
+
+var a = 4;
+var b = 8;
+var c = a+b;
+print(c)
+
+// 十个数排序
+var n = [4,2,5,12,53,6,46,3,23,35]
+for i in 0..<n.count
+{
+    for j in i+1..<n.count
+    {
+        if(n[i]>n[j]){
+            var temp = n[j]
+            n[j] = n[i]
+            n[i] = temp
+        }
+    }
+}
+print(n)
